5a4b00cb8b964fa9fd264eb5169ea7b2e33f46e9
qdwang <wangqidong@gmail.com>
2022-08-22 15:25:51 +0800

    add dir module

--- src/writer/dir.rs
+++ src/writer/dir.rs
@@ -1,26 +1,35 @@
-use std::io;
 use super::*;
+use std::cell::RefCell;
+use std::fs::File;
+use std::io;
+use std::rc::Rc;
+use car_util::DirectoryItem;
 
-
-pub struct Dir<W : io::Write> {
-    next_writer: W
+pub struct Dir<W: io::Write> {
+    curr_file: Rc<RefCell<u64>>,
+    next_writer: W,
 }
 
-impl<W : io::Write> io::Write for Dir<W> {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        Ok(buf.len())
+impl<W: io::Write> Dir<W> {
+    pub fn new(next_writer: W) -> Self {
+        Dir { curr_file: Rc::new(RefCell::new(0)), next_writer }
     }
 
-    fn flush(&mut self) -> io::Result<()> {
+    pub fn walk_write(&mut self, dir_items: &[DirectoryItem]) -> io::Result<()> {
+        for item in dir_items {
+            match item {
+                DirectoryItem::File(_, path, id) => {
+                    *self.curr_file.borrow_mut() = *id;
+                    let mut file = File::open(path)?;
+                    io::copy(&mut file, &mut self.next_writer)?;
+                    self.next_writer.flush()?;
+                }
+                DirectoryItem::Directory(_, sub_dir_items) => {
+                    self.walk_write(sub_dir_items)?;
+                }
+            }
+        }
+
         Ok(())
-    }
-}
-
-impl<W : io::Write> ChainWrite<W> for Dir<W> {
-    fn next(self) -> W {
-        self.next_writer
-    }
-    fn next_mut(&mut self) -> &mut W {
-        &mut self.next_writer
     }
 }
